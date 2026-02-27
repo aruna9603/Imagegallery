ff481a9fbd5c9eabfc768602a33d1c31e7e3299b
aruna9603 <arunaanbazhagan03@gmail.com>
2026-02-27 20:18:42 +0900

    updated image file

--- src/components/image.jsx
+++ src/components/image.jsx
@@ -1,7 +1,7 @@
 function Imagecard(props) {
   return (
     <div className="bg-red-600 w-500px h-500px p-5 text-center rounded">
-      <img className="w-80 h-56 shadow-md shadow-black hover:translate-y-2" src={props.img} alt="photo" />
+      <img className="w-80 h-56 shadow-md shadow-black hover:translate-y-2" src={props.img} alt="marvel hero" />
       <h1 className="m-3 text-blue-950 text-2xl">{props.disc}</h1>
     </div>
   )
